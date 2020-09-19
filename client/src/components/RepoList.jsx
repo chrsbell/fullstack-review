aaf8cc4c74c9e9bd5dec795956b994f1141cce7f
Chris Bell <cb.camellia@gmail.com>
2020-09-19 13:54:34 -0700

    Implement repo table

--- client/src/components/RepoList.jsx
+++ client/src/components/RepoList.jsx
@@ -1,13 +1,39 @@
 import React from 'react';
 
-let RepoList = ({repos}) => {
-  console.log(repos);
-  return (
-    <div>
-      <h4> Repo List Component </h4>
-      There are {repos.length} repos.
-    </div>
-  );
+let RepoList = ({ repos }) => {
+  // console.log(repos);
+  if (!repos.length) {
+    return null;
+  } else {
+    return (
+      <div>
+        <table>
+          <tbody>
+          <tr>
+            <th>Repo Name</th>
+            <th>URL</th>
+            <th>Owner</th>
+            <th>Stars</th>
+            <th>Watchers</th>
+          </tr>
+            {repos.map((repo, i) => {
+              // give each field a unique key
+              let numKeys = 6;
+              return (
+              <tr key={i*numKeys}>
+                <td key={(i*numKeys)+1}>{repo.name}</td>
+                <td key={(i*numKeys)+2}>{repo.html_url}</td>
+                <td key={(i*numKeys)+3}>{repo.owner}</td>
+                <td key={(i*numKeys)+4}>{repo.stargazers_count}</td>
+                <td key={(i*numKeys)+5}>{repo.watchers_count}</td>
+              </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 };
 
 export default RepoList;
