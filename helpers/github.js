a9b85d6bfda844fc6fb1fd3286b521b32c0aa3ed
Chris Bell <cb.camellia@gmail.com>
2020-09-18 18:04:43 -0700

    Implement getReposByUsername

--- helpers/github.js
+++ helpers/github.js
@@ -1,19 +1,43 @@
 const axios = require('axios');
 const config = require('../config.js');
+const _ = require('underscore');
 
-let getReposByUsername = (/* TODO */) => {
+let getReposByUsername = (username) => {
   // TODO - Use the axios module to request repos for a specific
   // user from the github API
 
-  // The options object has been provided to help you out, 
+  // The options object has been provided to help you out,
   // but you'll have to fill in the URL
+  console.log(`Username: ${username}`);
+
   let options = {
-    url: 'FILL ME IN',
+    method: 'get',
+    url: `https://api.github.com/users/${username}/repos`,
     headers: {
       'User-Agent': 'request',
       'Authorization': `token ${config.TOKEN}`
     }
   };
+
+  // return data as a Promise
+  return axios(options)
+  .then(res => {
+    // can access JSON data from data key of res
+    filtered = _.map(res.data, (repo) => {
+      // filter necessary data from repo list
+      repo = _.pick(repo, 'id', 'name', 'html_url', 'owner', 'stargazers_count', 'watchers_count')
+      repo.owner = username;
+      return repo;
+      // add login field only from owner sub collection
+      // _.extend(repo, _.pick(repo.owner, 'login'));
+      // delete repo.owner;
+    });
+    return filtered;
+
+  })
+  .catch(err => {
+    console.log(`Couldn't complete request!`);
+  })
 
 }
 
--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const github = require('../helpers/github.js');
+
 let app = express();
 
 let db = require('../database/index.js');
@@ -9,16 +11,18 @@ app.use(express.static(__dirname + '/../client/dist'));
 app.use(bodyParser.json());
 
 app.post('/repos', (req, res) => {
-  // TODO - your code here!
   // This route should take the github username provided
   // and get the repo information from the github API, then
   // save the repo information in the database
+  github.getReposByUsername(req.body.term)
+  .then(data => {
+    console.log(data);
+  });
   res.sendStatus(201);
-  console.log(req.body);
+  // console.log(req.body);
 });
 
 app.get('/repos', (req, res) => {
-  // TODO - your code here!
   // This route should send back the top 25 repos
 });
 
